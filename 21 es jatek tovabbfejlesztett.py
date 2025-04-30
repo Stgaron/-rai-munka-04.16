47beecfbaafe8f0122bed3a40709c221f3eb6528
Stgaron <aron.csabai10@gmail.com>
2025-04-30 13:32:52 +0200

    tovabbfejlesztes

--- 21 es jatek tovabbfejlesztett.py
+++ 21 es jatek tovabbfejlesztett.py
@@ -1,41 +1,41 @@
 from random import randint
 print("Üdvözöllek a 21 es játékba")
 print("A cél az,hogy a szám legyen minnél közelebb a 21-hez,aki túllépi az veszt azonnal")
-print("Ebben a játékban a lapok száma 1 és 10 között lesz")
-print("3 kártyád van")
+print("Ebben a játékban a lapok száma 2 és 11 között lesz")
+print("2 kártyád van")
 print("A kártyáidból el is tudsz tüntetni")
 def jatek_ember():
-    if jatekmod == "ember":
+    if jatekmod == "e":
         print("Akkor Ember ellen szeretnél játszani")
         print("Az első játékos:")
-        kartyak1 = [randint(1, 10) for _ in range(3)]
+        kartyak1 = [randint(2, 11) for _ in range(2)]
         print(f"A kártyáid száma = {kartyak1}")
         print(f"A kártyáid összege = {sum(kartyak1)}")
-        kivonas = int(input("Ha szeretnél eltüntetni egy kártyát írd be a számát, ha semmit akkor 4-et: "))
-        if kivonas in [1, 2, 3]:
-            vegsoszam = sum(kartyak1) - kartyak1[kivonas - 1]
-        elif kivonas == 4:
-            vegsoszam = sum(kartyak1)
-        else:
-            while kivonas < 1 or kivonas > 4:
-                print("Ide mást mint (1,2,3,4)")
-                kivonas = int(input("Ha szeretnél eltüntetni egy kártyát írd be a számát, ha semmit akkor 4-et: "))
-            vegsoszam = sum(kartyak1)
+        if sum(kartyak1) > 16:
+            ujrakero=input("Szeretnél húzni még egy kártyát?,(i,n):")
+            if ujrakero=="i":
+                kartyak1.append(randint(2, 11))
+                sum(kartyak1)
+                print(kartyak1)
+            else:
+                print("")
+
+        vegsoszam = sum(kartyak1)
         print(f"A végső számod = {vegsoszam}")
         print("A második játékos:")
-        kartyak2 = [randint(1, 10) for j in range(3)]
+        kartyak2 = [randint(2, 11) for j in range(2)]
         print(f"A kártyáid száma = {kartyak2}")
         print(f"A kártyáid összege = {sum(kartyak2)}")
-        kivonas = int(input("Ha szeretnél eltüntetni egy kártyát, írd be a számát, ha nem akkor írd azt, hogy 4: "))
-        if kivonas in [1, 2, 3]:
-            masodikvegsoszam = sum(kartyak2) - kartyak2[kivonas - 1]
-        elif kivonas == 4:
-            masodikvegsoszam = sum(kartyak2)
-        else:
-            while kivonas < 1 or kivonas > 4:
-                print("Ide mást írtál mint  (1,2,3,4)")
-                kivonas = int(input("Ha szeretnél eltüntetni egy kártyát, írd be a számát, ha nem akkor írd azt, hogy 4: "))
-            masodikvegsoszam = sum(kartyak2)
+        if sum(kartyak2) > 16:
+            ujrakero=input("Szeretnél húzni még egy kártyát?,(i,n):")
+            if ujrakero=="i":
+                kartyak1.append(randint(2, 11))
+                sum(kartyak2)
+                print(f"A kártyáid száma = {kartyak2}")
+            else:
+                print("")
+
+        masodikvegsoszam = sum(kartyak2)
         print(f"A végső számod = {masodikvegsoszam}")
         if vegsoszam > 21 and masodikvegsoszam > 21:
             print("Mindkét játékos túllépte a 21-et, döntetlen!")
@@ -51,19 +51,19 @@ def jatek_ember():
             print("Döntetlen")
 
 def robot():
-    if jatekmod =="robot":
+    if jatekmod =="r":
         print("idk")
 jatek = 'i'
 while jatek == 'i':
-    jatekmod = input("Kérem válasszon játékmódot (ember,robot): ")
-    if jatekmod == "ember":
+    jatekmod = input("Kérem válasszon játékmódot ember vagy robot(e,r): ")
+    if jatekmod == "e":
         jatek_ember()
-    elif jatekmod == "robot":
+    elif jatekmod == "r":
         robot()
     else:
         print("Nem értelmezhető játékmód!")
 
-    if jatekmod in ("ember", "robot"):
+    if jatekmod in ("e", "r"):
         vegekerdes = ""
         while vegekerdes not in ('i', 'n'):
             vegekerdes = input("Szeretne új játékot játszani? (i/n): ")
